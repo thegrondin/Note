fef3d912a8632e0c448c429b0bba0e1452921ed6
Thomas Dion-Grondin <tomtom9461@gmail.com>
2022-10-01 13:51:46 -0400

    Add antd to project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component} from "react";
+import Layout from "./components/Layout";
 
 class App extends Component{
   render(){
     return(
-      <div className="App">
-        <h1> Hello, World! 2</h1>
-      </div>
+      <Layout />
     );
   }
 }
